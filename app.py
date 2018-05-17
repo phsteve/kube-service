3c4fa46c47f7c62fd0b48bbdfe4ece1ef7eaa18a
Steve Katz <steve@teacherspayteachers.com>
2018-05-17 14:43:34 -0400

    Initial commit, app and dockerfile

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/api/some_endpoint')
+def index():
+    return flask.jsonify({'data': 'some data'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
