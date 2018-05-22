f69b49bb9ec900a5e0d67bf419f011e2ff296085
Steve Katz <steve@teacherspayteachers.com>
2018-05-22 14:45:54 -0400

    Add / endpoint

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route('/api/some_endpoint')
-def index():
+def some_endpoint():
     return flask.jsonify({'data': 'some data'})
+
+@app.route('/')
+def index():
+    return 'hello from /'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
